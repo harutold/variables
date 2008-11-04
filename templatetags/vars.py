04a96adf5fbf3747af062da7d615780836f59c2c
FHen <FHen@a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7>
2008-11-04 21:11:00 +0000

    Улучшил переменные:
    + резолвятся параметры тега
    + можно передавать несколько параметров. Они будут конкатенироваться.
    
    git-svn-id: http://sociallab.unfuddle.com/svn/sociallab_sandbox/django-addons/variables@53 a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7

--- templatetags/vars.py
+++ templatetags/vars.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django import template
+from django.template import resolve_variable
 from variables.models import *
 
 register = template.Library()
@@ -8,18 +9,19 @@ register = template.Library()
 @register.tag(name="variable")
 def variable(parser, token):
     tokens = token.contents.split()
-    if len(tokens) != 2:
-        raise template.TemplateSyntaxError(u"'%r' tag requires 1 argument." % tokens[0])
+    if len(tokens) < 2:
+        raise template.TemplateSyntaxError(u"'%r' tag requires at least 1 argument." % tokens[0])
     
-    return VariableNode(tokens[1])
+    return VariableNode(tokens[1:])
 
 class VariableNode(template.Node):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, names):
+        self.names = names
 
     def render(self, context):
         try:
-            var = Variable.objects.get(name=self.name)
+            name = ''.join([unicode(resolve_variable(x, context)) for x in self.names])
+            var = Variable.objects.get(name=name)
         except Variable.DoesNotExist:
             raise Exception(u"""
                             Переменная с названием «%s» не определена.
